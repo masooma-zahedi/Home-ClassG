9ed1259161ab5c1f80694ab884f87c002f56d582
masooma zahedi <masooma.zahedi1@gmail.com>
2025-10-02 19:21:53 -0500

    add Bouncing Ball to site

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { CardAlphaCompo } from "../sections/alphabet/CardAlphaCompo";
 import BrokenGlass from "../sections/alphabet/BrokenGlass";
 import WordGameWithCategories from "../sections/alphabet/WordGameWithCategories";
+import BouncingBalls from "../sections/alphabet/BouncingBalls";
 
 export default function Home() {
   return (<>
     <CardAlphaCompo/> 
     {/* <BrokenGlass/> */}
+    <BouncingBalls/>
     <WordGameWithCategories initialCategory="وسایل مدرسه" />
+
   </>)
 }
